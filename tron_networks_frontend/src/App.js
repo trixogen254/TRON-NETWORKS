9a6a45ad1648630a438de92b74a9efe5ed0bdbda
trixogen254 <fiddymzea@gmail.com>
2024-06-02 12:24:15 +0800

    uh

--- tron_networks_frontend/src/App.js
+++ tron_networks_frontend/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/tron_networks_frontend">
       <div className="App">
         <Routes>
           <Route path="/" element={<Register />} />
