0d4915db5764c2a593b8888156e71f436c69f691
trixogen254 <fiddymzea@gmail.com>
2024-06-07 13:40:43 +0300

    RN

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(cors({
 app.use((req, res, next) => {
   console.log('Request URL:', req.originalUrl);
   console.log('Request Method:', req.method);
-  console.log('Request Headers:', req.headers);
+  console.log('Request Headers:', JSON.stringify(req.headers, null, 2));
   next();
 });
 
